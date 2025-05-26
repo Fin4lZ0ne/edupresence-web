0ebfc8375ab5aef3d525f3dbe716dc93720bba25
root <root@localhost.localdomain>
2025-05-26 14:31:22 +0700

    create: forget password

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,11 +2,23 @@
 set -e
 
 chown -R www-data:www-data storage
+
 if [ ! -d "vendor" ]; then
     composer install --no-interaction --optimize-autoloader
 fi
 
 php artisan key:generate
+
+# Buat folder storage/app/public jika belum ada
+mkdir -p storage/app/public
+
+# Bersihkan symlink jika sudah ada dan salah
+if [ -e "public/storage" ]; then
+    rm -rf public/storage
+fi
+
+# Buat symlink public/storage
+php artisan storage:link || true
 
 # Install Nodejs Deps
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
@@ -14,11 +26,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# source ~/.bashrc
-
 nvm install --lts
 npm install && npm run build
-
-php artisan storage:link
 
 exec apache2-foreground
